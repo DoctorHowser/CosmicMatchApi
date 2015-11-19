6aad6f3f3218d852c84e43054a7a456728c2cd0c
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-11-19 16:46:17 -0600

    add first last email to user schema, dom

--- client/scripts/user.js
+++ client/scripts/user.js
@@ -7,6 +7,14 @@ $(document).ready(function(){
         success: function(data){
             console.log(data);
             $("#welcome").text("Welcome, " +  data.username);
+            appendDom(data)
         }
     });
 });
+
+function appendDom (userData){
+    var el = "<h1>" + userData.firstName + " " + userData.lastName + "</h1>" +
+            "<h3>Email : " + userData.email + "</h3>";
+
+    $('#user').append(el);
+}
