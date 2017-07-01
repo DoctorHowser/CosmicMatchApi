258137b74f5780f42498f7aba474850d40a326b6
Dane Smith <dsmith@Danes-iMac.local>
2017-07-01 18:22:35 -0500

    make basic route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -53,6 +53,10 @@ router.post("/comparison", function (req, res, next) {
         })
 });
 
+router.get("/", (req, res, next) => {
+    res.send("Hello from Cosmic Match!");
+});
+
 router.get("/*", function (req, res, next) {
     var file = req.params[0] || 'views/index.html';
     res.sendFile(path.join(__dirname, "../public", file));
