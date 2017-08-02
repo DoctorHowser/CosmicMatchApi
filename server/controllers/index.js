18e09af0c8ddcbaf7f8588eac0a9ffa37588a129
Dane Smith <dsmith@Danes-iMac.local>
2017-08-01 20:13:49 -0500

    cors update

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -8,7 +8,7 @@ router.all('/*', function (req, res, next){
     res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Headers', '*');
 	if ( req.method === 'OPTIONS' ) {
 		res.writeHead(200);
 		res.end();
