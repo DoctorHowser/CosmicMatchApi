0fd3912ff91d18015b0de894707ac5d1488e9e81
Dane Smith <dsmith@Danes-iMac.domain>
2018-01-15 20:05:29 -0600

    change how port works

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ var index = require('./controllers/index');
 var userRouter = require('./routes/userRouter')
 
 // App Set //
-app.set("port", (process.env.PORT || 5000));
+let port= process.env.PORT || 5000;
 
 
 app.use(cookieParser());
@@ -23,6 +23,6 @@ app.use('/', index);
 
 
 // Listen //
-app.listen(app.get("port"), function(){
-   console.log("Listening on port: " + app.get("port"));
+app.listen(port, function(){
+   console.log("Listening on port: " + port);
 });
